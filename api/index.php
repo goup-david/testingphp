d8639ddfd7a0f7189c23fb4b91d53382691b6ea3
goup-david <49403907+goup-david@users.noreply.github.com>
2022-04-27 16:40:53 +0100

    some more changes

--- api/index.php
+++ api/index.php
@@ -1,5 +1,14 @@
+<html>
+    <head></head>
+
+<body>
+
+<link rel="stylesheet" href="style.css">
+
 <div class="testbackground">
     <a href="/anotherpage">Another Page</a>
 
     <h1>Hello there!</h1>
 </div>
+</body>
+</html>
