47db5c8cc723fbe3b5ae3c566ff85665d2c4e657
goup-david <49403907+goup-david@users.noreply.github.com>
2022-05-03 14:07:38 +0100

    revert changes

--- api/index.php
+++ api/index.php
@@ -1,7 +1,1 @@
-<?php if (str_starts_with($_SERVER['REQUEST_URI'], '/another-page') {
-  require __DIR__ . '/../another-page/index.php';
-} else {
-  require __DIR__ . '/../index.php';
-} ?>
-
-// <?php include __DIR__ . '/../index.php'; ?>
+<?php include __DIR__ . '/../index.php'; ?>
