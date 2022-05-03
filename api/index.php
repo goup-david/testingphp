6e7b6d4a559e157e89d0cf19f9364d4dd5f9a48a
goup-david <49403907+goup-david@users.noreply.github.com>
2022-05-03 14:05:36 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,1 +1,7 @@
-<?php include __DIR__ . '/../index.php'; ?>
+<?php if (str_starts_with($_SERVER['REQUEST_URI'], '/another-page') {
+  require __DIR__ . '/../another-page/index.php';
+} else {
+  require __DIR__ . '/../index.php';
+} ?>
+
+// <?php include __DIR__ . '/../index.php'; ?>
